ec63509b63886dd35c641cb0caa4c4684a8fa531
hande <handeeeozzzz@gmail.com>
2024-02-03 13:49:20 +0300

    tek veri alındı

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Label from "./Label";
+import Button from "./Button";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    info: "dvxd",
+    info2: "dg",
+    userInput: "",
+    displayedInput:"",
+    displayedInputs: []
+  };
+  handleInputChange = (event) => {
+    this.setState({ userInput: event.target.value });
+  };
+  handleButtonClick = () => {
+    this.setState({ displayedInput: this.state.userInput }); // Butona tıklandığında input değerini displayedInput state'ine aktarın
+     
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleInputChange}   
+        />
+       <Button infot={this.state.displayedInput}  onclick={this.handleButtonClick}/>
+       <Label infot2={this.state.displayedInput} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Label.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Label extends Component {
+  render() {
+    return (
+      <div>
+        
+        <p>kul deg {this.props.infot2}</p>
+      </div>   
+    )
+  }
+}
